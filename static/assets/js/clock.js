0268d4508490dc6deb713d594801e17f3d68ab79
Bruce Burge <bruce@bruceburge.com>
2016-04-14 22:38:16 -0500

    cleaned up clock js, and reformated timetest

--- static/assets/js/clock.js
+++ static/assets/js/clock.js
@@ -1,23 +1,43 @@
 $(function(){
 
     var temp = 0.0;
+    var weatherCondition ="N/A";
     var serverNow;
 	var clock = $('#clock');
     var day = $('#day');
-    var now;
+    var progressbar = $('#dayProgressBar');
 
-	$.getJSON("../UIdata", function(data) {
-       temp = data.temperature;
-       serverNow = data.date;
-	});
+    var fetchDataCounter = 0;
 
+    function getUiData(){
+        $.getJSON("../UIdata", function(data) {
+           temp = data.temperature;
+           serverNow = data.date;
+           //weatherCondition = data.weather;
+        });
+    }
 
+    getUiData();
 	(function update_time(){
+        var percent = (Math.round((moment().hours()/24)*100)) + '%'
+        progressbar.width(percent);
+        progressbar.text(percent);
 
 		clock.text(moment().format("HH:mm:ss")).slabText();
 		day.text(moment().format("dddd"));
 		day.append(" "+temp+"&deg;F");
 		//console.debug(now);
+		//update conditions in UI every ten seconds
+		if(fetchDataCounter >= 60)
+		{
+		    getUiData();
+		    fetchDataCounter = 0;
+		}
+		else
+		{
+		    fetchDataCounter++;
+		}
+
 		setTimeout(update_time, 1000);	
 	})();	
 });
